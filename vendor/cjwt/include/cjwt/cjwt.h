06279d74404beb29611bb3ab6d59f5999f069eda
Qi Gao <qi.gao2012@gmail.com>
2026-09-21 22:42:44 +0800

    refactor(cjwt): consume SaltsUtils JSON header

--- vendor/cjwt/include/cjwt/cjwt.h
+++ vendor/cjwt/include/cjwt/cjwt.h
@@ -6,7 +6,7 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#include <turbo_parser.h>
+#include <json_parser.h>
 
 /*----------------------------------------------------------------------------*/
 /*                                   Macros                                   */

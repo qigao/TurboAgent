5d718b850247ceda958709152772406c88c4fcf4
Qi Gao <qi.gao2012@gmail.com>
2026-09-22 02:21:23 +0800

    refactor: use canonical tstr type

--- langchain/src/turbo_agent_request_replay.c
+++ langchain/src/turbo_agent_request_replay.c
@@ -250,7 +250,7 @@ CXX_C_API json_value_t *turbo_agent_request_build_canonical_messages_with_option
 
   if (agent->context_projection_active) {
     char *summary_json;
-    tstr_t summary_text;
+    tstr summary_text;
     json_value_t *summary_message;
 
     if (!agent->context_summary) {

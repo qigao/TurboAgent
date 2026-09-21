2f61ca6ce4b4be99106d3308e5f6cc35523e7baf
Qi Gao <qi.gao2012@gmail.com>
2026-09-22 02:19:37 +0800

    refactor: use canonical tstr type

--- langchain/src/turbo_agent_util_internal.h
+++ langchain/src/turbo_agent_util_internal.h
@@ -13,7 +13,7 @@ extern "C" {
 #endif
 
 /* String Utilities */
-CXX_C_API tstr_t turbo_agent_util_strdup(const char *src);
+CXX_C_API tstr turbo_agent_util_strdup(const char *src);
 
 /* Data JsonValue Utilities */
 CXX_C_API int turbo_agent_util_json_value_object_set_string(

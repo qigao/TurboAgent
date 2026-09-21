99297ad30d8ed57f0cef94db095909574c00457c
Qi Gao <qi.gao2012@gmail.com>
2026-09-22 00:04:49 +0800

    build: use TurboAgent public API header

--- langchain/include/turbo_agent_harness.h
+++ langchain/include/turbo_agent_harness.h
@@ -1,8 +1,7 @@
 #ifndef TURBO_AGENT_HARNESS_H
 #define TURBO_AGENT_HARNESS_H
 
-#include <platform.h>
-#include <turbo_error.h>
+#include <turbo_agent_api.h>
 #include <turbo_thread.h>
 
 #include "turbo_agent_app.h"
